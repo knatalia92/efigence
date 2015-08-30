ca4d92ecd324bbbc5f53ff8d7852c677f2b2167c
knatalia92 <k.natalia92@gmail.com>
2015-08-31 00:40:50 +0200

    Details

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    /*** GOOGLE MAPS ***/
     
     var position = [27.1959739, 78.02423269999997];
 
@@ -23,6 +25,36 @@ $(document).ready(function() {
         });
     }
 
-
     google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
+     /*** VIDEO ***/
+
+    var content = $('#video');
+    var video = $('#video')[0];
+    var play = $('.play-pause');
+
+     content.on('click', function() {
+        if (video.paused) {
+            video.play();
+            play.hide();
+        } else {
+            video.pause();
+            play.show();
+        }
+     });
+
+
+      /*** MENU ***/
+
+      var menu = $('.minified-menu a');
+      var close = $('.icon-close');
+      var cover = $('.cover-menu-content');
+
+      menu.on('click', function() {
+        cover.fadeIn();
+      });
+
+      close.on('click', function() {
+        cover.fadeOut();
+      })
  });
