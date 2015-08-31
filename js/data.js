9cfd2a6e624994e8af42cb60ae730ce6082f63cd
knatalia92 <k.natalia92@gmail.com>
2015-08-31 17:14:52 +0200

    Load content via ajax

---
+++ js/data.js
@@ -0,0 +1,71 @@
+this.onload = function() {
+  var url = 'http://camp.efigence.com/camp/api/places';
+
+  function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ('withCredentials' in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != 'undefined'){ 
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+  }
+
+     var xhr = createCORSRequest('GET', url);
+     if(xhr) {
+        xhr.onload = function() {
+          if(xhr.status == 200) {
+            var arr = JSON.parse(xhr.responseText);
+            loadArray(arr);
+
+          } else if (xhr.status == 422) {
+            console.log('Unprocessable entity ');
+          } else if (xhr.status == 500) {
+            console.log('Server crashed for some <%= reason %> ');
+          }
+        };
+     }
+      xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+      xhr.send(null);
+
+  /********** LOAD CONTENT TO ARRAY ***************/
+
+  function loadArray(arr) {
+      var article = 0;
+      for(var i = 0; i < Object.keys(arr.places).length; i++) {
+        article += '<div class="row"><div class="col-md-3 col-md-offset-1 col-sm-3 col-xs-8"><img src="../public/img/gallery1.jpg" alt="miejscowka1"></div><div class="col-md-4 col-sm-6 col-xs-12"><article>';
+        article += '<h1 class="news-title">' + arr.places[i].place + '</h1>';
+        article += '<h2 class="news-details">' + arr.places[i].district + '</h2>';
+        article += '<div class="grade">';
+        for(var j = 0; j < parseInt(arr.places[i].starts); j++) {
+          article += '<span class="icon" data-icon="&#xe60c;"></span>';
+        }       
+        article += '</div>';
+        article += '<p class="description">' + arr.places[i].description + '</p>';
+        article += '<a href="#" class="read-more"> Szczegóły </a>';
+        article += '</article></div>';
+        article += '<div class="col-md-1 col-sm-1 options">';
+        if( arr.places[i].wifi) {
+          article += '<a href="#" data-toggle="tooltip" title="Wifi" class="icon" data-icon="&#xe600;"></a>';
+        }
+        article += '</div>';
+        article += '<div class="col-md-2 col-sm-3 col-xs-6 price-details">'
+        article += '<h1 class="rating"> Bardzo dobry '+ arr.places[i].score + '/10 </h1>'
+        article += '<h2 class="opinions-number"> Ocena na podstawie ' + arr.places[i].opinion_count + ' opinii </h2>'
+        article += '<h3 class="before-discount"> ' + arr.places[i].oldprice + '00,00 PLN </h3>'
+        article += '<h4 class="after-discount"> ' + arr.places[i].price + '00,00 <span class="grey"> PLN </span> </h4>'
+        article += '<h5 class="nights"> Cena za 3 noce </h5>'
+        article += '<button class="btn"> Zarezerwuj teraz </button>'
+        article += '</div>';
+        article += '</div>';
+      }
+
+    var content =  document.querySelector('.information-table .container-fluid');
+    content.innerHTML = article;
+
+  /********** PAGINATE ***************/
+  }
+};
--- js/script.js
+++ js/script.js
@@ -1,60 +1,24 @@
-$(document).ready(function() {
+this.onload = function() {
 
-    /*** GOOGLE MAPS ***/
-    
-    var position = [27.1959739, 78.02423269999997];
+  var video = document.getElementById('video');
+  var play = document.querySelector('.play-pause');
 
-    function showGoogleMaps() {
-        var latLng = new google.maps.LatLng(position[0], position[1]);
-
-        var mapOptions = {
-            zoom: 16, 
-            streetViewControl: false, 
-            scaleControl: true, 
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            center: latLng
-        };
-
-        map = new google.maps.Map(document.getElementById('googlemaps'), mapOptions);
-
-        marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            draggable: false,
-            animation: google.maps.Animation.DROP
-        });
+  play.addEventListener('click', function() {
+     if (video.paused) {
+        video.play();
+        play.style.display = 'none';
+      }
+  });
+  
+  video.addEventListener('click', function() {
+    if (video.play) {
+      video.pause();
+      play.style.display = 'inline-block';
     }
-
-    google.maps.event.addDomListener(window, 'load', showGoogleMaps);
-
-     /*** VIDEO ***/
-
-    var content = $('#video');
-    var video = $('#video')[0];
-    var play = $('.play-pause');
-
-     content.on('click', function() {
-        if (video.paused) {
-            video.play();
-            play.hide();
-        } else {
-            video.pause();
-            play.show();
-        }
-     });
+  });
+}
 
 
-      /*** MENU ***/
 
-      var menu = $('.minified-menu a');
-      var close = $('.icon-close');
-      var cover = $('.cover-menu-content');
 
-      menu.on('click', function() {
-        cover.fadeIn();
-      });
 
-      close.on('click', function() {
-        cover.fadeOut();
-      })
- });
