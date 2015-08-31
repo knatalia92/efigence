47cd8571571d7e6ffbce5359a5d79f054e573d33
knatalia92 <k.natalia92@gmail.com>
2015-08-31 17:40:54 +0200

    Basic pagination

--- js/data.js
+++ js/data.js
@@ -20,6 +20,7 @@ this.onload = function() {
           if(xhr.status == 200) {
             var arr = JSON.parse(xhr.responseText);
             loadArray(arr);
+            paginate(arr);
 
           } else if (xhr.status == 422) {
             console.log('Unprocessable entity ');
@@ -34,9 +35,9 @@ this.onload = function() {
   /********** LOAD CONTENT TO ARRAY ***************/
 
   function loadArray(arr) {
-      var article = 0;
+      var article = '';
       for(var i = 0; i < Object.keys(arr.places).length; i++) {
-        article += '<div class="row"><div class="col-md-3 col-md-offset-1 col-sm-3 col-xs-8"><img src="../public/img/gallery1.jpg" alt="miejscowka1"></div><div class="col-md-4 col-sm-6 col-xs-12"><article>';
+        article += '<div class="row"><div class="col-md-3 col-md-offset-1 col-sm-3 col-xs-8"><img src="../public/img/gallery' + Math.floor(6*Math.random() + 1) + '.jpg" alt="miejscowka"></div><div class="col-md-4 col-sm-6 col-xs-12"><article>';
         article += '<h1 class="news-title">' + arr.places[i].place + '</h1>';
         article += '<h2 class="news-details">' + arr.places[i].district + '</h2>';
         article += '<div class="grade">';
@@ -53,7 +54,7 @@ this.onload = function() {
         }
         article += '</div>';
         article += '<div class="col-md-2 col-sm-3 col-xs-6 price-details">'
-        article += '<h1 class="rating"> Bardzo dobry '+ arr.places[i].score + '/10 </h1>'
+        article += '<h1 class="rating">' + opinionRate(parseInt(arr.places[i].score)) + ' '+ arr.places[i].score + '/10 </h1>'
         article += '<h2 class="opinions-number"> Ocena na podstawie ' + arr.places[i].opinion_count + ' opinii </h2>'
         article += '<h3 class="before-discount"> ' + arr.places[i].oldprice + '00,00 PLN </h3>'
         article += '<h4 class="after-discount"> ' + arr.places[i].price + '00,00 <span class="grey"> PLN </span> </h4>'
@@ -66,6 +67,36 @@ this.onload = function() {
     var content =  document.querySelector('.information-table .container-fluid');
     content.innerHTML = article;
 
-  /********** PAGINATE ***************/
+    function opinionRate(num) {
+      var opinion = '';
+      num = parseFloat(num);
+
+      if(num <= 3.0) {
+        opinion = 'Słaby';
+      } else if (num > 3.0 && num <= 5.0) {
+        opinion = 'Średni';
+      } else if (num > 5.0 && num <= 7.0) {
+        opinion = 'Dobry';
+      } else if (num > 7.0 && num <= 9.0) {
+        opinion = 'Bardzo dobry';
+      } else if (num > 9.0) {
+        opinion = 'Rewelacyjny';
+      }
+
+      return opinion;
+    }
   }
+
+  /********** PAGINATION ***************/
+  function paginate(arr) {
+    var content = '';
+    content += '<ul>';
+    var list = document.querySelector('.list');
+    for(var i = 0; i < parseInt(arr.total_pages); i++) {
+      content += '<li>' + (i+1) + '</li>';
+    }
+    content += '</ul>'
+    list.innerHTML = content;
+  }
+  
 };
